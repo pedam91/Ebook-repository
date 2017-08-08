15b476150f586d23844bbf1e879aa22cb95e7e1d
Petar Damjanovic <p.damjanovic@levi9.com>
2017-08-08 19:00:28 +0200

    Logged in user should see category which he subscribed for, if he ever did subscribe

--- src/main/java/com/pdamjanovic/controller/HomeController.java
+++ src/main/java/com/pdamjanovic/controller/HomeController.java
@@ -1,14 +1,20 @@
 package com.pdamjanovic.controller;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.pdamjanovic.entities.Category;
 import com.pdamjanovic.service.CategoryService;
+import com.pdamjanovic.service.UserService;
+import com.pdamjanovic.util.LoggedInUser;
 
 @Controller
 public class HomeController {
@@ -18,11 +24,29 @@ public class HomeController {
 	@Autowired
 	CategoryService categoryService;
 
+	@Autowired
+	UserService userService;
+
 	@GetMapping("/")
 	public String homePage(Map<String, Object> model) {
 
 		logger.debug("HOME PAGE METHOD - GET");
-		model.put("categories", categoryService.findAll());
+
+		List<Category> listOfCategories = null;
+
+		Object loggedInUser = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (loggedInUser != null && loggedInUser instanceof LoggedInUser) {
+			Category usersCategory = userService.findById(((LoggedInUser) loggedInUser).getId()).getCategory();
+			if (usersCategory != null) {
+				listOfCategories = Arrays.asList(usersCategory);
+			}
+		}
+
+		if (listOfCategories == null) {
+			listOfCategories = (List<Category>) categoryService.findAll();
+		}
+
+		model.put("categories", listOfCategories);
 
 		return "home";
 	}
